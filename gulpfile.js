eded687f29b36a3e7ccb3ceec2eae71705bd0cfc
Jesse Hallett <jesse@sitr.us>
2015-02-25 14:09:15 -0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp')
+  , browserify = require('browserify')
+  , transform = require('vinyl-transform')
+;
+
+gulp.task('browserify', function() {
+    var browserified = transform(function(filename) {
+        var b = browserify(filename);
+        return b.bundle();
+    });
+
+    return gulp.src(['./src/*.js'])
+        .pipe(browserified)
+        .pipe(gulp.dest('./dist'));
+});
